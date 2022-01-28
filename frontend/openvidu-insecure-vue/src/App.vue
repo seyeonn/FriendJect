130acbf7a99d56be6052e912a1c8297b977e2bff
김주향 <ilove_13@naver.com>
2022-01-28 09:59:18 +0900

    에러 파일 수정
    Former-commit-id: 6fdb34474a4dbdbf5c4fe36bfcd17a1b6ea3a2e9

---
+++ frontend/openvidu-insecure-vue/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- 메인이 되는 부분 -->
+
+  <div>
+    <!-- 반응형 메인 이미지 코드!! 주석 처리 되었으나, 재사용 할거같으니 지우지 마세용!!! -->
+    <!-- <b-img :src="require('./assets/images/main.png')" fluid-gro></b-img> -->
+    <Header />
+    <div id="main-container" class="container">
+      <!-- 자주 바뀌는 영역 -->
+      <router-view></router-view>
+    </div>
+  </div>
+  <!-- Content here -->
+</template>
+
+<script>
+import Header from "./components/layout/Header.vue";
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+};
+</script>
+
+
+<!-- <style lang="scss">
+	@import "@/assets/main.scss";
+
+</style> -->
