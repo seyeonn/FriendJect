16c636d59740549ac6faf6074cbae267c7245eb0
정채은 <ddj03340@naver.com>
2022-02-03 00:36:24 +0900

    [feat] 파일 하나 저장하기
    Former-commit-id: 80da3e4543187206a7f8391b66bf1d91686e2c52

---
+++ backend-java/src/main/java/com/ssafy/api/service/FileInfoService.java
@@ -0,0 +1,19 @@
+package com.ssafy.api.service;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.ssafy.db.entity.FileInfo;
+
+public interface FileInfoService {
+	FileInfo save(MultipartFile file) throws IOException;
+	//  public Resource load(String filename);
+//
+//  public void deleteAll();
+//
+//  public Stream<Path> loadAll();
+}
--- backend-java/src/main/java/com/ssafy/db/entity/FileInfo.java
+++ backend-java/src/main/java/com/ssafy/db/entity/FileInfo.java
@@ -1,27 +1,58 @@
 package com.ssafy.db.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@Table(name = "file")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FileInfo {
-  private String name;
-  private String url;
+	
+	@Id @GeneratedValue
+	@Column(name = "file_id")
+	private Long id;
+	
+	@Column(name = "file_name")
+	private String fileName;
+	
+//	@Column(name = "file_origin_name")
+//	private String fileOriginName;
+//	
+//	@Column(name = "file_path")
+//	private String filePath;
+	
+	@Lob
+	@Column(name = "data")
+	private byte[] data;
+	
+	@Column(name = "content_type")
+	private String contentType;
 
-  public FileInfo(String name, String url) {
-    this.name = name;
-    this.url = url;
-  }
-
-  public String getName() {
-    return this.name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getUrl() {
-    return this.url;
-  }
-
-  public void setUrl(String url) {
-    this.url = url;
-  }
+	@Builder
+	public FileInfo(String fileName, byte[] data, String contentType) {
+		this.fileName = fileName;
+		this.data = data;
+		this.contentType = contentType;
+	}
+	
+//	@Builder
+//	public FileInfo(Long id, String fileName, String fileOriginName, String filePath, String contentType) {
+//		this.id = id;
+//		this.fileName = fileName;
+//		this.fileOriginName = fileOriginName;
+//		this.filePath = filePath;
+//		this.contentType = contentType;
+//	}
+	
 }
