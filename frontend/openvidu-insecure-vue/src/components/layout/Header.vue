67dde058a80f078e164c114ca21db57401bdbdfb
김주향 <ilove_13@naver.com>
2022-02-08 02:25:28 +0900

    [feat] 카카오 로그인 프론트엔드 연동
    Former-commit-id: 3e82e1f80e857f16e712865d84db0a47643eca30

--- frontend/openvidu-insecure-vue/src/components/layout/Header.vue
+++ frontend/openvidu-insecure-vue/src/components/layout/Header.vue
@@ -19,9 +19,12 @@
       <div>
         <a href="#" title="Close" class="modal-close">Close</a>
         <div style="text-align:center;"><h1>3초만에 시작하기</h1></div>
-        <div>
-          여기에 카카오톡으로 로그인 연결
-          <button v-on:click="login"> 로그인 됐다고 가정 </button>
+        <div style="text-align:center; margin-top:50px;">
+          <a
+            href="https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=e1a7f29f3266d25acd09415836291442&redirect_uri=http://localhost:8080/kakao"
+          >
+            <img alt="kakao-button" src="@/assets/images/kakao_login.png" />
+          </a>
         </div>
         <!-- <div><small>Check out</small></div> -->
       </div>
