dc146009b0661996e5471a0d85d4c462c5a15287
김주향 <ilove_13@naver.com>
2022-02-08 03:55:52 +0900

    [feat] 페이지 emit 처리
    Former-commit-id: 4f30aaf8fc57933fa66ad890f82b38f1fb1ab1ec

--- frontend/openvidu-insecure-vue/src/components/layout/Center.vue
+++ frontend/openvidu-insecure-vue/src/components/layout/Center.vue
@@ -1,15 +1,44 @@
 <template>
   <div class="main_center" style="height:100%">
-    
+    <b-row>
+      <b-col class="box" style="margin-right:10%">
+        <button @click="setTab('StudyRoom')">Study</button>
+      </b-col>
+      <b-col class="box" >
+        <button @click="setTab('MeetingRoom')">Meeting</button>
+        </b-col>
+    </b-row>
+
+    <b-row>
+      <b-col class="box" style="margin-right:10%">
+        <button @click="setTab('KanbanBoard')">Project</button>
+      </b-col>
+      <b-col class="box">
+        <button @click="setTab('ConsultRoom')">Consult</button>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      currentTab: "Center",
+    };
+  },
+  methods: {
+    setTab: function(tab) {
+      this.$emit("emitTab", tab);
+    },
+  },
+};
 </script>
 
 <style>
-
+.box {
+  padding-top: 18%;
+  padding-bottom: 8%;
+  margin-left: 18%;
+}
 </style>
