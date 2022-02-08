1f8f4cbfa2f8cd8429bda2d2dbd771f5a09de7e6
seyeonn <ansse17@gmail.com>
2022-02-08 15:03:01 +0900

    [comment] 주석 추가
    Former-commit-id: 368a3c14d65d5b9bfc4ce05db64c91cc5aa7f26e

--- backend-java/src/main/java/com/ssafy/api/service/StudyRoomServiceImpl.java
+++ backend-java/src/main/java/com/ssafy/api/service/StudyRoomServiceImpl.java
@@ -15,6 +15,7 @@ public class StudyRoomServiceImpl implements StudyRoomService{
 	@Autowired
 	StudyRoomRepository studyroomRepository;
 
+	/* 열람실에 입장한 유저의 정보를 가져와서 해당 userid가 0인지 확인(새로 들어온 유저 확인) 후 db에 entryuser 저장*/
 	@Override
 	public Studyroom entryUser(Studyroom userEntryInfo) {
 		if(studyroomRepository.countByUserid(userEntryInfo.getUserid()) == 0) {
@@ -28,12 +29,13 @@ public class StudyRoomServiceImpl implements StudyRoomService{
 		}
 	}
 
+	/* userid로 user 조회 */
 	@Override
 	public Studyroom getUserByUserId(String userid) {
 		return studyroomRepository.findByUserid(userid).get();
 	}
 
-
+	/* userid로 user를 찾아서 time 업데이트 */
 	@Override
 	   public Studyroom saveTime(String userid, int time) {
 	      Studyroom user = getUserByUserId(userid);
@@ -41,6 +43,7 @@ public class StudyRoomServiceImpl implements StudyRoomService{
 	      return studyroomRepository.save(user);
 	   }
 
+	/* db에 저장된 데이터를 time 기준으로 내림차순으로 정렬한 후 가장 상위에 있는 데이터만 조회 */
 	@Override
 	   public String getStudyBest() {
 	      
