ce7fcaf8bdd8c7094d4b8523808af9b429275a53
정채은 <ddj03340@naver.com>
2022-02-09 17:11:15 +0900

    [feat] 파일 컨트롤러 응답 객체 변경
    Former-commit-id: e1ee0a20e0c97c13ab825938382164ccad2b28ae

--- backend-java/src/main/java/com/ssafy/api/response/ResponseFactory.java
+++ backend-java/src/main/java/com/ssafy/api/response/ResponseFactory.java
@@ -10,29 +10,37 @@ public class ResponseFactory {
     public static ResponseEntity<? extends BaseResponseBody> ok() {
         return ResponseEntity.status(HttpStatus.OK).body(BaseResponseBody.of(200, "성공", new EmptyDTO(true)));
     }
+    
+    public static ResponseEntity<? extends BaseResponseBody> ok(Object object) {
+    	return ResponseEntity.status(HttpStatus.OK).body(BaseResponseBody.of(200, "성공", object));
+    }
 
     public static ResponseEntity<? extends BaseResponseBody> created() {
         return ResponseEntity.status(HttpStatus.CREATED).body(BaseResponseBody.of(201, "생성됨", new EmptyDTO(true)));
     }
-
-    public static ResponseEntity<? extends BaseResponseBody> notFound() {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(BaseResponseBody.of(404, "찾을 수 없음", new EmptyDTO(true)));
+    
+    public static ResponseEntity<? extends BaseResponseBody> noContent() {
+    	return ResponseEntity.status(HttpStatus.NO_CONTENT).body(BaseResponseBody.of(204, "존재하지 않음", new EmptyDTO(true)));
+    }
+    
+    public static ResponseEntity<? extends BaseResponseBody> unauthorized() {
+    	return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(BaseResponseBody.of(401, "권한이 없음", new EmptyDTO(true)));
     }
 
     public static ResponseEntity<? extends BaseResponseBody> forbidden() {
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(BaseResponseBody.of(403, "금지됨", new EmptyDTO(true)));
     }
-
-    public static ResponseEntity<? extends BaseResponseBody> unauthorized() {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(BaseResponseBody.of(401, "권한이 없음", new EmptyDTO(true)));
-    }
-
-    public static ResponseEntity<? extends BaseResponseBody> noContent() {
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(BaseResponseBody.of(204, "존재하지 않음", new EmptyDTO(true)));
+    
+    public static ResponseEntity<? extends BaseResponseBody> notFound() {
+    	return ResponseEntity.status(HttpStatus.NOT_FOUND).body(BaseResponseBody.of(404, "찾을 수 없음", new EmptyDTO(true)));
     }
 
     public static ResponseEntity<? extends BaseResponseBody> conflict() {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(BaseResponseBody.of(409, "충돌", new EmptyDTO(true)));
+    }
+    
+    public static ResponseEntity<? extends BaseResponseBody> expectationFailed() {
+    	return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(BaseResponseBody.of(417, "요청 거부됨", new EmptyDTO(true)));
     }
 
     public static ResponseEntity<? extends BaseResponseBody> internalServerError() {
