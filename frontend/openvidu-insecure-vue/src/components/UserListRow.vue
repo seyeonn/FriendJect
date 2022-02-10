8b42dec819e325fda31b56b05c49fdc2684ea075
정채은 <ddj03340@naver.com>
2022-02-10 15:17:26 +0900

    [feat] 비디오와 소리 온앤오프 기능
    Former-commit-id: 42aced72c6162eba7201dce75776211fafbd55ae

---
+++ frontend/openvidu-insecure-vue/src/components/UserListRow.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="user">
+    <img src="https://randomuser.me/api/portraits/men/1.jpg" class="user-img" />
+    <div class="username">
+      {{ sub }}
+      <div class="user-status"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    // connection: "",
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/style/main-new.scss";
+</style>
