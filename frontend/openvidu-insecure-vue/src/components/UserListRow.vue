7437d3de32dda9cccb39c4e66834b5f8066d947e
정채은 <ddj03340@naver.com>
2022-02-11 20:40:56 +0900

    init

---
+++ frontend/openvidu-insecure-vue/src/components/UserListRow.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="user">
+    <!-- user profile_url api로 추가 예정 -->
+    <img src="https://randomuser.me/api/portraits/men/1.jpg" class="user-img" />
+    <div class="username">
+      {{ clientData }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserListRow",
+
+  props: {
+    streamManager: Object,
+  },
+
+  computed: {
+    clientData() {
+      const { clientData } = this.getConnectionData();
+      return clientData;
+    },
+  },
+
+  methods: {
+    getConnectionData() {
+      const { connection } = this.streamManager.stream;
+      return JSON.parse(connection.data);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/style/main-new.scss";
+</style>
---
+++ frontend/openvidu-insecure-vue/src/components/projectroom/FileList.vue
@@ -0,0 +1,142 @@
+<template>
+  <b-row class="m-5">
+    <b-col cols="7 mt-5 ml-5"
+      ><div id="app" class="grid p-8">
+        <h3><b>File Table</b> 📄</h3>
+        <table>
+          <thead>
+            <tr>
+              <th :class="{ sorted: sort === 'name' }" @click="sortBy('name')">
+                Name
+              </th>
+              <th :class="{ sorted: sort === 'type' }" @click="sortBy('type')">
+                Uploader
+              </th>
+              <th :class="{ sorted: sort === 'size' }" @click="sortBy('size')">
+                Size
+              </th>
+              <th
+                :class="{ sorted: sort === 'updatedAt' }"
+                @click="sortBy('updatedAt')"
+              >
+                Updated
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <file-list-row
+              v-for="(file, index) in sortedFiles()"
+              :key="index"
+              v-bind="file"
+            ></file-list-row>
+          </tbody>
+        </table>
+
+        <div class="controls">
+          <button class="btn btn-sm" :disabled="!hasPage(-1)" @click="prevPage">
+            ←
+          </button>
+          <button class="btn btn-sm" :disabled="!hasPage(1)" @click="nextPage">
+            →
+          </button>
+        </div>
+      </div>
+    </b-col>
+    <b-col cols="4 p-5 center" class="rounded border border-warning m-4"
+      ><h4><b>파일 올리기</b>👇</h4>
+      <file-upload />
+    </b-col>
+  </b-row>
+</template>
+<script>
+import FileListRow from "@/components/projectroom/child/FileListRow";
+import FileUpload from "@/components/projectroom/FileUpload.vue";
+import { getFileList } from "@/api/projectroom.js";
+
+export default {
+  name: "fileList",
+  components: {
+    FileListRow,
+    FileUpload,
+  },
+  data() {
+    return {
+      files: [],
+      sort: "updatedAt",
+      sortDir: "desc",
+      page: 0,
+      pageSize: 10,
+    };
+  },
+  created() {
+    getFileList(
+      (response) => {
+        console.log(response.data.data);
+        this.files = response.data.data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  },
+  mounted() {},
+  methods: {
+    sortedFiles() {
+      return this.files
+        .sort((a, b) => {
+          let dir = this.sortDir === "asc" ? 1 : -1;
+          if (a[this.sort] < b[this.sort]) {
+            return -1 * dir;
+          } else if (a[this.sort] > b[this.sort]) {
+            return 1 * dir;
+          } else {
+            return 0;
+          }
+        })
+        .filter((row, idx) => {
+          let s = this.page * this.pageSize;
+          let e = (this.page + 1) * this.pageSize;
+          return idx >= s && idx < e;
+        });
+    },
+    sortBy(s) {
+      if (s === this.sort) {
+        this.sortDir = this.sortDir === "asc" ? "desc" : "asc";
+      } else {
+        this.sortDir = "asc";
+      }
+      this.sort = s;
+    },
+    isActiveSort(s) {
+      return this.sort === s;
+    },
+    hasPage(dir) {
+      if (dir === -1 && this.page > 0) return true;
+      if (dir === 1 && (this.page + 1) * this.pageSize < this.files.length)
+        return true;
+      return false;
+    },
+    prevPage() {
+      if (this.hasPage(-1)) this.page--;
+    },
+    nextPage() {
+      if (this.hasPage(1)) this.page++;
+    },
+  },
+  computed: {
+    pageSizeModel: {
+      get() {
+        return this.pageSize;
+      },
+      set(v) {
+        this.pageSize = v;
+        this.page = 0;
+      },
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/style/projectroom/fileList.scss";
+</style>
