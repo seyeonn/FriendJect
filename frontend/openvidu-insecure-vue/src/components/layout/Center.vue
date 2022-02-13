128f7212a9f8f7d1fa6a382eed7ec087e8c7f7c2
seyeonn <ansse17@gmail.com>
2022-02-14 00:16:06 +0900

    [feat] 센터화면에 모달창 띄우기
    Former-commit-id: e5cd23fd7b4a86d9f956fb023348f4649add6b6f

--- frontend/openvidu-insecure-vue/src/components/layout/Center.vue
+++ frontend/openvidu-insecure-vue/src/components/layout/Center.vue
@@ -1,5 +1,24 @@
 <template>
   <div class="main_center" style="height:100%">
+    <div>
+    <!-- 아이스 브레이킹 -->
+    <div @click="icebraking">
+      <a href="#icebraking"><img src="https://i.imgur.com/TsyLttq.png" style="width: 15%; margin-left: 500px;"/></a>
+    </div>
+
+    <!-- 아이스 브레이킹 모달창 -->
+    <div id="icebraking" class="modal-window">
+      <div>
+        <a href="#" title="Close" class="modal-close">
+            <b-icon icon="x-circle-fill" scale="2" variant="danger"></b-icon>
+        </a>
+        <div class="timer">
+        
+        </div>
+        
+      </div>
+    </div>
+    </div>
     <b-row>
       <b-col class="box" style="margin-right:10%">
         <button @click="setTab('StudyRoom')">Study</button>
@@ -10,10 +29,10 @@
     </b-row>
 
     <b-row>
-      <b-col class="box" style="margin-right:10%">
+      <b-col class="box2" style="margin-right:10%">
         <button @click="setTab('ProjectRoom')">Project</button>
       </b-col>
-      <b-col class="box">
+      <b-col class="box2">
         <button @click="setTab('ConsultRoom')">Consult</button>
       </b-col>
     </b-row>
@@ -35,8 +54,19 @@ export default {
 };
 </script>
 
+<style lang="scss" scoped>
+@import "@/assets/style/icebraking.scss";
+</style>
+
 <style>
+@import url(https://fonts.googleapis.com/css?family=Open+Sans);
+
 .box {
+  padding-top: 5%;
+  padding-bottom: 7%;
+  margin-left: 18%;
+}
+.box2 {
   padding-top: 18%;
   padding-bottom: 8%;
   margin-left: 18%;
