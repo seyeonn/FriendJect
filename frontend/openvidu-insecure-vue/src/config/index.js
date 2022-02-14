c4c5f977980510c00f050f8697af1b0a9b516ad2
soralee <chalrysr@naver.com>
2022-02-14 14:55:17 +0900

    [feat] chat 구현
    Former-commit-id: f301baf6efc98086ca2fc3b7bc426e447fc3bc27

--- frontend/openvidu-insecure-vue/src/config/index.js
+++ frontend/openvidu-insecure-vue/src/config/index.js
@@ -1,4 +1,5 @@
 const API_BASE_URL = "http://localhost:8081/friendject/";
 const PJT_BASE_URL = "http://localhost:8081/projectroom/";
+const SIGNUP_URL = "http://localhost:8081/login/";
 
-export { API_BASE_URL, PJT_BASE_URL };
+export { API_BASE_URL, PJT_BASE_URL, SIGNUP_URL };
--- frontend/openvidu-insecure-vue/src/views/KakaoRedirect.vue
+++ frontend/openvidu-insecure-vue/src/views/KakaoRedirect.vue
@@ -7,6 +7,7 @@
 <script>
 import { mapActions, mapState } from "vuex";
 import axios from "axios";
+import { SIGNUP_URL } from "@/config";
 
 export default {
   computed: {
@@ -17,7 +18,7 @@ export default {
     kako_api: function() {
       axios
         // .post("http://i6b202.p.ssafy.io:8081/login", {               서버에 올릴때
-        .post("http://localhost:8081/login", {
+        .post(SIGNUP_URL, {
           code: this.$route.query.code,
         })
         .then((res) => {
@@ -37,10 +38,6 @@ export default {
   },
   mounted() {
     this.kako_api();
-    // 여기서 join , 즉 본인의 방을 생성하거나 참가하는 페이지로 이동합니다.
-    // 이때 카카오에서 받은 닉네임을 props 로 choice.vue 에 넘겨주세요.
-    // choice 에서 넘겨받은 닉네임을 participant 에 input 값으로 지정해주세요.
-    // 그러면 자동으로 닉네임 + 팀코드로 방에 접속됩니다.
 
     console.log(this.$route.query.code);
     this.$router.replace("/join");
