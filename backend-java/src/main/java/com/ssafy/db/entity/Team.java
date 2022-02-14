a4b2344fe7d884c0b00a82ce0145d574667be2b8
정채은 <ddj03340@naver.com>
2022-02-15 04:09:58 +0900

    [feat] 팀, (테스트용)멤버 엔티티 추가
    Former-commit-id: 82acba55243b45f08088c89cbd7046c67d9a0b8e

--- backend-java/src/main/java/com/ssafy/db/entity/Team.java
+++ backend-java/src/main/java/com/ssafy/db/entity/Team.java
@@ -7,8 +7,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
@@ -19,21 +17,21 @@ import lombok.Setter;
 
 @Entity
 @Getter @Setter
-@Table(name = "member")
+@Table(name = "team")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Member {
-	@Id	@GeneratedValue
-	@Column(name = "member_id")
-	private Long id; 
-	 
-	@Column(name = "username") 
-	private String username; 
-		
-	@ManyToMany
-	@JoinTable(name = "joined_team_list",
-		joinColumns = @JoinColumn(name = "member_id"),
-		inverseJoinColumns = @JoinColumn(name = "team_id"))
-	private List<Team> teams = new ArrayList<>();	
-	
+public class Team {
 
+	@Id @GeneratedValue
+	@Column(name="team_id")
+	private Long id;
+	
+	@Column(name="team_name")
+	private String name;
+	
+	@Column(name="team_number")
+	private Long teamNumber;
+	
+	@ManyToMany(mappedBy = "teams")
+	private List<Member> members = new ArrayList<>();
+	
 }
