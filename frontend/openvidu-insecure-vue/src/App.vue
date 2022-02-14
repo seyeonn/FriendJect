e88ad68499dffe1c1e81eb547f864f932a15b0b3
김주향 <ilove_13@naver.com>
2022-02-15 08:12:55 +0900

    [refactor] 라우터 구조 변경
    Former-commit-id: ce7058ad2effed0a5d55f5a34a1b67d3c76029e6

--- frontend/openvidu-insecure-vue/src/App.vue
+++ frontend/openvidu-insecure-vue/src/App.vue
@@ -5,7 +5,7 @@
     <!-- 반응형 메인 이미지 코드!! 주석 처리 되었으나, 재사용 할거같으니 지우지 마세용!!! -->
     <!-- <b-img :src="require('./assets/images/main.png')" fluid-gro></b-img> -->
     <!-- 자주 바뀌는 영역 -->
-      <Header />
+      <!-- <Header /> -->
     <body>
       <router-view></router-view>
     </body>
@@ -14,11 +14,11 @@
 </template>
 
 <script>
-import Header from "./components/layout/Header.vue";
+
 export default {
   name: "App",
   components: {
-   Header,
+  
   },
 };
 </script>
