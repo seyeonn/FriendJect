03ca00edd977f9769c85ceb7171be95260afab40
정채은 <ddj03340@naver.com>
2022-02-17 16:36:13 +0900

    tesst
    Former-commit-id: 30e1f5d5a4de1c5787abb5137450d681f213ed1b

--- frontend/openvidu-insecure-vue/src/components/layout/Header.vue
+++ frontend/openvidu-insecure-vue/src/components/layout/Header.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <b-container fluid class="bv-example-row" style="height:10%">
+    <b-container fluid class="bv-example-row" style="height: 10%">
       <b-row>
-        <b-col><img class="logo" src="@/assets/images/logo.png"/></b-col>
+        <b-col><img class="logo" src="@/assets/images/logo.png" /></b-col>
         <b-col cols="8"></b-col>
         <b-col>
           <b-button
@@ -29,7 +29,7 @@
         <div style="text-align: center"><h1>3초만에 시작하기</h1></div>
         <div style="text-align: center; margin-top: 50px">
           <a
-            href="https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=e1a7f29f3266d25acd09415836291442&redirect_uri=http://localhost:8080/kakao"
+            href="https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=e1a7f29f3266d25acd09415836291442&redirect_uri=https://i6b202.p.ssafy.io/kakao"
           >
             <!-- 서버에 올릴 때<a
             href="https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=e1a7f29f3266d25acd09415836291442&redirect_uri=https://i6b202.p.ssafy.io/kakao"
