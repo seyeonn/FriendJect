da8833ca37ccece6db7c032ad88baaab7a033616
김주향 <ilove_13@naver.com>
2022-02-17 20:46:51 +0900

    [bug] 수정
    Former-commit-id: f29d7fcef78e00abc2fd713e8c502d2729dcfbb4

--- frontend/openvidu-insecure-vue/src/routers/index.js
+++ frontend/openvidu-insecure-vue/src/routers/index.js
@@ -3,166 +3,143 @@ import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Room from "../views/Room.vue";
  import Search from "../views/Search.vue";
-// import KakaoLogin from "../views/KakaoRedirect.vue";
-// import Center from "../components/layout/Center.vue";
-// import Minime from "../views/Minime.vue";
-//consult room
-// import Consult from "../views/ConsultRoom.vue";
-// import PageNotFound from "../views/PageNotFound.vue";
+import KakaoLogin from "../views/KakaoRedirect.vue";
+import Center from "../components/layout/Center.vue";
+import Minime from "../views/Minime.vue";
+// consult room
+import Consult from "../views/ConsultRoom.vue";
+import PageNotFound from "../views/PageNotFound.vue";
 // // project room
-// import Project from "../views/Project.vue";
-// import MemoBoard from "../components/projectroom/MemoBoard.vue";
-// import FileList from "../components/projectroom/FileList.vue";
+import Project from "../views/Project.vue";
+import MemoBoard from "../components/projectroom/MemoBoard.vue";
+import FileList from "../components/projectroom/FileList.vue";
 
 // // meeting room
-// import Meeting from "../views/Meeting.vue";
+import Meeting from "../views/Meeting.vue";
 
 // // study room
-// import Study from "../views/Studyroom/Studyroom.vue";
+import Study from "../views/Studyroom/Studyroom.vue";
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    mode: "history",
-    path: "/room",
-    name: "Room",
-    component: Room,
-  },
-  {
-    path: "/",
-    name: "home",
-    component: Home,
-  },
-  {
-    path: "/main",
-    name: "main",
-    component: Search,
-  },
-];
 
-const router = new VueRouter({
-  mode: "history",
-  routes,
+export const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    //path : url 주소, component : 페이지에 보여질 컴포넌트
+    {
+      path: "/",
+      name: "/",
+      component: Home,
+    },
+    {
+      path: "/main",
+      name: "main",
+      component: Search,
+    },
+    {
+      path: "/minime",
+      name: "minime",
+      component: Minime,
+    },
+    {
+      path: "/room",
+      name: "room",
+      component: Room,
+      redirect: "room/main",
+      props: true,
+      children: [
+        {
+          path: "studyroom",
+          name: "studyroom",
+          component: Study,
+        },
+        {
+          path: "main",
+          name: "main",
+          component: Center,
+        },
+        {
+          path: "meetingroom",
+          name: "meetingroom",
+          component: Meeting,
+        },
+        {
+          path: "consultroom",
+          name: "consultroom",
+          component: Consult,
+        },
+        {
+          path: "projectroom",
+          name: "projectroom",
+          component: Project,
+          redirect: "projectroom/memos",
+          children: [
+            {
+              path: "memos",
+              name: "memoboard",
+              component: MemoBoard,
+            },
+            {
+              path: "files",
+              name: "filelist",
+              component: FileList,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      path: "/consultroom",
+      name: "consultroom",
+      component: Consult,
+    },
+    {
+      path: "/404",
+      name: "notFound",
+      component: PageNotFound,
+    },
+    {
+      path: "*",
+      redirect: "/404",
+    },
+    {
+      path: "/studyroom",
+      name: "studyroom",
+      component: Study,
+    },
+    {
+      path: "/kakao",
+      name: "kakao",
+      component: KakaoLogin,
+    },
+    {
+      path: "/projectroom",
+      name: "projectroom",
+      component: Project,
+      redirect: "/projectroom/memos",
+      children: [
+        {
+          path: "memos",
+          name: "memoboard",
+          component: MemoBoard,
+        },
+        {
+          path: "files",
+          name: "filelist",
+          component: FileList,
+        },
+      ],
+    },
+    {
+      path: "meetingroom",
+      name: "meetingroom",
+      component: Meeting,
+    },
+  ],
 });
 
 export default router;
-
-// export const router = new VueRouter({
-//   mode: "history",
-//   routes: [
-//     //path : url 주소, component : 페이지에 보여질 컴포넌트
-//     {
-//       path: "/",
-//       name: "/",
-//       component: Home,
-//     },
-//     {
-//       path: "/main",
-//       name: "main",
-//       component: Search,
-//     },
-//     {
-//       path: "/minime",
-//       name: "minime",
-//       component: Minime,
-//     },
-//     {
-//       path: "/room",
-//       name: "room",
-//       component: Room,
-//       redirect: "room/main",
-//       props: true,
-//       children: [
-//         {
-//           path: "studyroom",
-//           name: "studyroom",
-//           component: Study,
-//         },
-//         {
-//           path: "main",
-//           name: "main",
-//           component: Center,
-//         },
-//         {
-//           path: "meetingroom",
-//           name: "meetingroom",
-//           component: Meeting,
-//         },
-//         {
-//           path: "consultroom",
-//           name: "consultroom",
-//           component: Consult,
-//         },
-//         {
-//           path: "projectroom",
-//           name: "projectroom",
-//           component: Project,
-//           redirect: "projectroom/memos",
-//           children: [
-//             {
-//               path: "memos",
-//               name: "memoboard",
-//               component: MemoBoard,
-//             },
-//             {
-//               path: "files",
-//               name: "filelist",
-//               component: FileList,
-//             },
-//           ],
-//         },
-//       ],
-//     },
-//     {
-//       path: "/consultroom",
-//       name: "consultroom",
-//       component: Consult,
-//     },
-//     {
-//       path: "/404",
-//       name: "notFound",
-//       component: PageNotFound,
-//     },
-//     {
-//       path: "*",
-//       redirect: "/404",
-//     },
-//     {
-//       path: "/studyroom",
-//       name: "studyroom",
-//       component: Study,
-//     },
-//     {
-//       path: "/kakao",
-//       name: "kakao",
-//       component: KakaoLogin,
-//     },
-//     {
-//       path: "/projectroom",
-//       name: "projectroom",
-//       component: Project,
-//       redirect: "/projectroom/memos",
-//       children: [
-//         {
-//           path: "memos",
-//           name: "memoboard",
-//           component: MemoBoard,
-//         },
-//         {
-//           path: "files",
-//           name: "filelist",
-//           component: FileList,
-//         },
-//       ],
-//     },
-//     {
-//       path: "meetingroom",
-//       name: "meetingroom",
-//       component: Meeting,
-//     },
-//   ],
-// });
 
 // 전역 가드
 // https://router.vuejs.org/kr/guide/advanced/navigation-guards.html#%E1%84%8C%E1%85%A5%E1%86%AB%E1%84%8B%E1%85%A7%E1%86%A8-%E1%84%80%E1%85%A1%E1%84%83%E1%85%B3
