b6264f2de406d1f73e43c585d2da9a949969ee16
정채은 <ddj03340@naver.com>
2022-02-18 00:42:25 +0900

    [fix] 라우터 원상복구
    Former-commit-id: d485bdc8ef7be85974a9fff6d3291c8d4300ed70

--- frontend/openvidu-insecure-vue/src/router/index.js
+++ frontend/openvidu-insecure-vue/src/router/index.js
@@ -1,24 +1,24 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "../views/Home.vue";
-// import Room from "../views/Room.vue";
+import Room from "../views/Room.vue";
 import Search from "../views/Search.vue";
 import KakaoLogin from "../views/KakaoRedirect.vue";
-// import Center from "../components/layout/Center.vue";
-// import Minime from "../views/Minime.vue";
+import Center from "../components/layout/Center.vue";
+import Minime from "../views/Minime.vue";
 // consult room
-// import Consult from "../views/ConsultRoom.vue";
+import Consult from "../views/ConsultRoom.vue";
 import PageNotFound from "../views/PageNotFound.vue";
-// // // project room
-// import Project from "../views/Project.vue";
-// import MemoBoard from "../components/projectroom/MemoBoard.vue";
-// import FileList from "../components/projectroom/FileList.vue";
+// project room
+import Project from "../views/Project.vue";
+import MemoBoard from "../components/projectroom/MemoBoard.vue";
+import FileList from "../components/projectroom/FileList.vue";
 
-// // // meeting room
-// import Meeting from "../views/Meeting.vue";
+// meeting room
+import Meeting from "../views/Meeting.vue";
 
-// // // study room
-// import Study from "../views/Studyroom/Studyroom.vue";
+// study room
+import Study from "../views/Studyroom/Studyroom.vue";
 
 Vue.use(Router);
 
@@ -49,6 +49,105 @@ export default new Router({
       path: "*",
       redirect: "/404",
     },
+    {
+      path: "/minime",
+      name: "minime",
+      component: Minime,
+    },
+    {
+      path: "/room",
+      name: "room",
+      component: Room,
+      redirect: "room/main",
+      props: true,
+      children: [
+        {
+          path: "studyroom",
+          name: "studyroom",
+          component: Study,
+        },
+        {
+          path: "main",
+          name: "main",
+          component: Center,
+        },
+        {
+          path: "meetingroom",
+          name: "meetingroom",
+          component: Meeting,
+        },
+        {
+          path: "consultroom",
+          name: "consultroom",
+          component: Consult,
+        },
+        {
+          path: "projectroom",
+          name: "projectroom",
+          component: Project,
+          redirect: "projectroom/memos",
+          children: [
+            {
+              path: "memos",
+              name: "memoboard",
+              component: MemoBoard,
+            },
+            {
+              path: "files",
+              name: "filelist",
+              component: FileList,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      path: "/consultroom",
+      name: "consultroom",
+      component: Consult,
+    },
+    {
+      path: "/404",
+      name: "notFound",
+      component: PageNotFound,
+    },
+    {
+      path: "*",
+      redirect: "/404",
+    },
+    {
+      path: "/studyroom",
+      name: "studyroom",
+      component: Study,
+    },
+    {
+      path: "/kakao",
+      name: "kakao",
+      component: KakaoLogin,
+    },
+    {
+      path: "/projectroom",
+      name: "projectroom",
+      component: Project,
+      redirect: "/projectroom/memos",
+      children: [
+        {
+          path: "memos",
+          name: "memoboard",
+          component: MemoBoard,
+        },
+        {
+          path: "files",
+          name: "filelist",
+          component: FileList,
+        },
+      ],
+    },
+    {
+      path: "meetingroom",
+      name: "meetingroom",
+      component: Meeting,
+    },
   ],
 });
 
@@ -56,116 +155,7 @@ export default new Router({
 //   mode: 'history',
 //   routes: [
 //     //path : url 주소, component : 페이지에 보여질 컴포넌트
-//     {
-//       path: "/",
-//       name: "/",
-//       component: Home,
-//     },
-//     {
-//       path: "/main",
-//       name: "main",
-//       component: Search,
-//     },
-//     {
-//       path: "/minime",
-//       name: "minime",
-//       component: Minime,
-//     },
-//     {
-//       path: "/room",
-//       name: "room",
-//       component: Room,
-//       redirect: "room/main",
-//       props: true,
-//       children: [
-//         {
-//           path: "studyroom",
-//           name: "studyroom",
-//           component: Study,
-//         },
-//         {
-//           path: "main",
-//           name: "main",
-//           component: Center,
-//         },
-//         {
-//           path: "meetingroom",
-//           name: "meetingroom",
-//           component: Meeting,
-//         },
-//         {
-//           path: "consultroom",
-//           name: "consultroom",
-//           component: Consult,
-//         },
-//         {
-//           path: "projectroom",
-//           name: "projectroom",
-//           component: Project,
-//           redirect: "projectroom/memos",
-//           children: [
-//             {
-//               path: "memos",
-//               name: "memoboard",
-//               component: MemoBoard,
-//             },
-//             {
-//               path: "files",
-//               name: "filelist",
-//               component: FileList,
-//             },
-//           ],
-//         },
-//       ],
-//     },
-//     {
-//       path: "/consultroom",
-//       name: "consultroom",
-//       component: Consult,
-//     },
-//     {
-//       path: "/404",
-//       name: "notFound",
-//       component: PageNotFound,
-//     },
-//     {
-//       path: "*",
-//       redirect: "/404",
-//     },
-//     {
-//       path: "/studyroom",
-//       name: "studyroom",
-//       component: Study,
-//     },
-//     {
-//       path: "/kakao",
-//       name: "kakao",
-//       component: KakaoLogin,
-//     },
-//     {
-//       path: "/projectroom",
-//       name: "projectroom",
-//       component: Project,
-//       redirect: "/projectroom/memos",
-//       children: [
-//         {
-//           path: "memos",
-//           name: "memoboard",
-//           component: MemoBoard,
-//         },
-//         {
-//           path: "files",
-//           name: "filelist",
-//           component: FileList,
-//         },
-//       ],
-//     },
-//     {
-//       path: "meetingroom",
-//       name: "meetingroom",
-//       component: Meeting,
-//     },
-//   ],
+
 // });
 
 // 전역 가드
