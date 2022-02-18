fa26d2bf6c27fa9fb43b4085d60d360ed27a7f1a
정채은 <ddj03340@naver.com>
2022-02-18 02:11:03 +0000

    [fix] 파일 다운 이름으로 하기
    Former-commit-id: 8ebba9e1106a467def18dd795522b91a5eb57936

--- frontend/openvidu-insecure-vue/src/components/projectroom/FileList.vue
+++ frontend/openvidu-insecure-vue/src/components/projectroom/FileList.vue
@@ -34,7 +34,7 @@
             ></file-list-row> -->
             <tr
               class="list__tr"
-              @click.prevent="downloadFile(file.id)"
+              @click.prevent="downloadFile(file.id, file.name)"
               v-for="(file, index) in files"
               :key="index"
             >
@@ -93,6 +93,7 @@ export default {
       returnSize: "",
       modifiedAt: "",
       uploader: "",
+      name: "",
     };
   },
   created() {
@@ -187,7 +188,8 @@ export default {
       var arr = new Array("정채은", "김주향", "안세연", "이소라", "송진섭");
       return arr[Math.floor(Math.random() * arr.length)];
     },
-    downloadFile(id) {
+    downloadFile(id, name) {
+      this.name = name;
       getFileInfo(
         id,
         (response) => {
