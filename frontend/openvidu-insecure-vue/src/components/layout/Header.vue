ecd75955584da5420fc5754ac6373718c628c0b7
정채은 <ddj03340@naver.com>
2022-02-24 01:39:28 +0900

    [chore] server name 변경
    Former-commit-id: 0cb2acd1330bd88d8d509f651242f1e528ad1a44

--- frontend/openvidu-insecure-vue/src/components/layout/Header.vue
+++ frontend/openvidu-insecure-vue/src/components/layout/Header.vue
@@ -32,12 +32,12 @@
         <a href="#" title="Close" class="modal-close">Close</a>
         <div style="text-align: center"><h1>3초만에 시작하기</h1></div>
         <div style="text-align: center; margin-top: 50px">
-          <a
+          <!-- 서버에 올릴 때 <a
             href="https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=e1a7f29f3266d25acd09415836291442&redirect_uri=http://localhost:8080/kakao"
-          >
-            <!-- 서버에 올릴 때 <a
-            href="https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=e1a7f29f3266d25acd09415836291442&redirect_uri=https://i6b202.p.ssafy.io/kakao"
           >-->
+          <a
+            href="https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=e1a7f29f3266d25acd09415836291442&redirect_uri=https://i6b202.p.ssafy.io/kakao"
+          >
             <img alt="kakao-button" src="@/assets/images/kakao_login.png" />
           </a>
         </div>
